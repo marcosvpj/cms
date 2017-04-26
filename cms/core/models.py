841fe6152e713648cac9f415f457da220964a0f8
Marcos V. Pagelkopf Jr <marcos@grupow.com.br>
2017-04-26 16:34:39 -0300

    Models relations

--- cms/core/models.py
+++ cms/core/models.py
@@ -7,7 +7,7 @@ class Category(models.Model):
 
 class Tag(models.Model):
     name = models.CharField('nome', max_length=100)
-    # relation with category
+    category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL)
 
 
 class Content(models.Model):
@@ -16,7 +16,7 @@ class Content(models.Model):
     content = models.TextField('conteudo', blank=True)
     position = models.IntegerField('posicao')
     is_listable = models.IntegerField('listavel')
-    # relation with tag
+    tags = models.ManyToManyField(Tag)
 
 
 class Card(models.Model):
@@ -25,3 +25,4 @@ class Card(models.Model):
     subtitle = models.CharField('subtitulo', max_length=255)
     content = models.TextField('conteudo', blank=True)
     position = models.IntegerField('posicao')
+    parent = models.ForeignKey(Content, on_delete=models.CASCADE)
