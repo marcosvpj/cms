fd65df36a1248a0cfcf7f52d0b8bb368a9c1c12a
Marcos V. Pagelkopf Jr <marcos@grupow.com.br>
2017-04-26 16:12:05 -0300

    Criar models iniciais

--- cms/core/models.py
+++ cms/core/models.py
@@ -1,5 +1,27 @@
 from django.db import models
 
-class Category(models.Model):
-    name = models.CharField('nome', max_length=255)
 
+class Category(models.Model):
+    name = models.CharField('nome', max_length=100)
+
+
+class Tag(models.Model):
+    name = models.CharField('nome', max_length=100)
+    # relation with category
+
+
+class Content(models.Model):
+    name = models.CharField('nome', max_length=100)
+    title = models.CharField('titulo', max_length=255)
+    content = models.TextField('conteudo', blank=True)
+    position = models.IntegerField('posicao')
+    is_listable = models.IntegerField('listavel')
+    # relation with tag
+
+
+class Card(models.Model):
+    name = models.CharField('nome', max_length=100)
+    title = models.CharField('titulo', max_length=255)
+    subtitle = models.CharField('subtitulo', max_length=255)
+    content = models.TextField('conteudo', blank=True)
+    position = models.IntegerField('posicao')
